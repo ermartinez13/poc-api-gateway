fb6d51a2bef9ba82c098cf023da7315a5a1b09db
Erick Martinez <ermartinez13@gmail.com>
2023-09-20 10:17:30 -0400

    scaffold foo service

--- foo/index.js
+++ foo/index.js
@@ -1,1 +1,23 @@
-console.log("foo");
+import { log } from "node:console";
+import http from "node:http";
+
+const server = http.createServer((req, res) => {
+  if (req.method === "GET" && req.url === "/") {
+    const body = {
+      data: "foo",
+    };
+    res.writeHead(200, { "Content-Type": "application/json" });
+    res.end(JSON.stringify(body));
+  } else if (req.method !== "GET") {
+    res.writeHead(405, { "Content-Type": "text/plain" });
+    res.end("405 Method not allowed");
+  } else {
+    res.writeHead(404, { "Content-Type": "text/plain" });
+    res.end("404 Not found");
+  }
+});
+
+const port = 3002;
+server.listen(port, () => {
+  log(`Foo service running on port ${port}`);
+});
