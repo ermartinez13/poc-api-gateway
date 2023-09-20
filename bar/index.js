c3c203811e8fb14e32a2ac15aa3c5acca8801268
Erick Martinez <ermartinez13@gmail.com>
2023-09-20 10:10:14 -0400

    scaffold bar service

--- bar/index.js
+++ bar/index.js
@@ -1,1 +1,21 @@
-console.log("bar");
+import http from "node:http";
+
+const server = http.createServer((req, res) => {
+  if (req.method === "GET" && req.url === "/") {
+    const body = "bar";
+    res.writeHead(200, { "Content-Type": "text/plain" });
+    res.end(body);
+  } else if (req.method !== "GET") {
+    res.writeHead(405, { "Content-Type": "text/plain" });
+    res.end("405 Method not allowed");
+  } else {
+    res.writeHead(404, { "Content-Type": "text/plain" });
+    res.end("404 Not found");
+  }
+});
+
+const port = 3001;
+
+server.listen(port, () => {
+  console.log(`Bar service running on port ${port}`);
+});
